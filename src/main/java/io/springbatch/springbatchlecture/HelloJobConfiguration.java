fceba0105d5a2bb91c9b0ece34358dadfa40341d
Yongkyu Park <dydrbdjdsla@gmail.com>
2023-09-08 07:52:49 +0900

    Initial commit

---
+++ src/main/java/io/springbatch/springbatchlecture/HelloJobConfiguration.java
@@ -0,0 +1,78 @@
+package io.springbatch.springbatchlecture;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class HelloJobConfiguration {
+
+    @Bean
+    public DataSource batchDataSource() {
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+                .addScript("/org/springframework/batch/core/schema-h2.sql")
+                .generateUniqueName(true).build();
+    }
+
+    @Bean
+    public JdbcTransactionManager batchTransactionManager(DataSource dataSource) {
+        return new JdbcTransactionManager(dataSource);
+    }
+
+    @Bean
+    public Job helloJob(JobRepository jobRepository, Step helloStep1, Step helloStep2) {
+        return new JobBuilder("helloJob", jobRepository)
+                .start(helloStep1)
+                .next(helloStep2)
+                .build();
+    }
+
+    @Bean
+    public Step helloStep1(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("helloStep1", jobRepository)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(" =======================");
+                        System.out.println(" >> Hello Spring Batch!!");
+                        System.out.println(" =======================");
+
+                        return RepeatStatus.FINISHED;
+                    }
+                }, platformTransactionManager)
+                .build();
+    }
+
+    @Bean
+    public Step helloStep2(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("helloStep2", jobRepository)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println(" =======================");
+                        System.out.println(" >> step2 was executed");
+                        System.out.println(" =======================");
+
+                        return RepeatStatus.FINISHED;
+                    }
+                }, platformTransactionManager)
+                .build();
+    }
+}
